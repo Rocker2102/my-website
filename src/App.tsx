c1751cf0bec2d7d48e7ff7f7ed99b68b0268f3c2
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-26 07:33:43 +0530

    Add theming support

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { ThemeProvider } from '@mui/system';
+import { createTheme } from '@mui/material/styles';
+
 import NavbarComponent from './components/NavbarComponent';
 
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
@@ -5,38 +8,55 @@ import './App.css';
 import logo from './logo.svg';
 
 function App(): JSX.Element {
+    const theme = createTheme({
+        palette: {
+            mode: 'dark'
+        },
+        components: {
+            MuiAppBar: {
+                styleOverrides: {
+                    colorPrimary: {
+                        backgroundColor: 'rgba(48, 48, 52, 0.92)'
+                    }
+                }
+            }
+        }
+    });
+
     return (
         <div className="App">
             <Router>
-                <NavbarComponent />
+                <ThemeProvider theme={theme}>
+                    <NavbarComponent />
 
-                <Switch>
-                    <Route exact path="/projects">
-                        <header className="App-header">Projects</header>
-                    </Route>
-                    <Route exact path="/about">
-                        <header className="App-header">About</header>
-                    </Route>
-                    <Route exact path="/contact">
-                        <header className="App-header">Contact</header>
-                    </Route>
-                    <Route>
-                        <header className="App-header">
-                            <img src={logo} className="App-logo" alt="logo" />
-                            <p>
-                                Edit <code>src/App.tsx</code> and save to reload.
-                            </p>
-                            <a
-                                className="App-link"
-                                href="https://reactjs.org"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Learn React
-                            </a>
-                        </header>
-                    </Route>
-                </Switch>
+                    <Switch>
+                        <Route exact path="/projects">
+                            <header className="App-header">Projects</header>
+                        </Route>
+                        <Route exact path="/about">
+                            <header className="App-header">About</header>
+                        </Route>
+                        <Route exact path="/contact">
+                            <header className="App-header">Contact</header>
+                        </Route>
+                        <Route>
+                            <header className="App-header">
+                                <img src={logo} className="App-logo" alt="logo" />
+                                <p>
+                                    Edit <code>src/App.tsx</code> and save to reload.
+                                </p>
+                                <a
+                                    className="App-link"
+                                    href="https://reactjs.org"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Learn React
+                                </a>
+                            </header>
+                        </Route>
+                    </Switch>
+                </ThemeProvider>
             </Router>
         </div>
     );
