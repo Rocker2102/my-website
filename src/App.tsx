8ace98e397e85636c935098b75a3ea6d1d65ebb5
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-26 06:44:00 +0530

    Use react-router-dom to navigate between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import NavbarComponent from './components/NavbarComponent';
+
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import logo from './logo.svg';
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Router>
+                <NavbarComponent />
+
+                <Switch>
+                    <Route exact path="/projects">
+                        <header className="App-header">Projects</header>
+                    </Route>
+                    <Route exact path="/about">
+                        <header className="App-header">About</header>
+                    </Route>
+                    <Route exact path="/contact">
+                        <header className="App-header">Contact</header>
+                    </Route>
+                    <Route>
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <p>
+                                Edit <code>src/App.tsx</code> and save to reload.
+                            </p>
+                            <a
+                                className="App-link"
+                                href="https://reactjs.org"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Learn React
+                            </a>
+                        </header>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
