0b6948913fe971b742ac555243a6c1fdeab3d3bc
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-28 04:17:35 +0530

    Add custom root component to wrap all content

---
+++ src/components/ContentBox.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import { styled, experimental_sx as sx } from '@mui/system';
+
+const ContentBox = styled(Box)(
+    sx({
+        pt: 8,
+        pb: { xs: 10, md: 4 },
+        mx: { xs: 2, sm: 6, md: '6rem', lg: '8rem', xl: '10rem' }
+    })
+);
+
+export default ContentBox;
