b2cc9405cbfdfce96661d13872275d7e70b5cf25
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-29 14:54:35 +0530

    Add shared/appSettings.ts
    -> all dynamic app settings are stored here

---
+++ src/shared/appSettings.ts
@@ -0,0 +1,10 @@
+export const FONTS = {
+    para: 'LexendDeca Light',
+    title: 'Raleway',
+    general: 'LexendDeca',
+    openSans: 'Open Sans'
+};
+
+export const USER_DATA = {
+    name: 'Ankush Yadav'
+};
