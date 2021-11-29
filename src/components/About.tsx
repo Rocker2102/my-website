6bd57ee4508754cd36ef12cd70ba95eacd9c1715
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-29 13:28:11 +0530

    Add base layout for About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,28 @@
 import { FC } from 'react';
 
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Grow from '@mui/material/Grow';
+import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 
 import ContentBox from './ContentBox';
+import profileImg from '../shared/profile.jpg';
+
+/**
+ * Display picture dimension & units
+ * (use a 1:1 ratio picture for best results)
+ */
+const imgSize = 30,
+    imgUnit = 'vh';
+
+/* eslint-disable */
+const MainInfo = [
+    `I'm a full-stack web developer (mostly self-taught) and a student from Lucknow, India`,
+    `I'm currently building (and learning of course 😅), a cross-platform desktop application
+        using Electron.js & React`
+];
+/* eslint-enable */
 
 const About: FC = () => {
     return (
@@ -14,10 +32,37 @@ const About: FC = () => {
                     About Me
                 </Typography>
 
-                <Box>
-                    <Typography variant="h4" color="text.primary" fontFamily="LexendDeca Light">
-                        Under development
-                    </Typography>
+                <Box mt={4}>
+                    <Grid container spacing={3}>
+                        <Grid item textAlign="center" margin="auto">
+                            <Avatar
+                                src={profileImg}
+                                alt="Ankush Yadav"
+                                sx={{
+                                    width: `${imgSize}${imgUnit}`,
+                                    height: `${imgSize}${imgUnit}`
+                                }}
+                            />
+                        </Grid>
+                        <Grid
+                            item
+                            margin={{ xs: 'auto', md: 'initial' }}
+                            maxWidth={{ xs: 'initial', sm: '50vw' }}
+                            textAlign={{ xs: 'center', md: 'left' }}
+                        >
+                            {MainInfo.map((text, i) => (
+                                <Typography
+                                    key={i}
+                                    mb={2}
+                                    variant="h5"
+                                    color="text.primary"
+                                    fontFamily="LexendDeca Light"
+                                >
+                                    {text}.
+                                </Typography>
+                            ))}
+                        </Grid>
+                    </Grid>
                 </Box>
             </ContentBox>
         </Grow>
