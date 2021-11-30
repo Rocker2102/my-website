029c63afae172df1313b5fa46ad58c2265c7c063
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-30 11:49:38 +0530

    Add timeline data in aboutData.ts

--- src/shared/aboutData.ts
+++ src/shared/aboutData.ts
@@ -1,3 +1,5 @@
+import { FC } from 'react';
+
 import profileImg from '../images/profile.jpg';
 
 /* eslint-disable */
@@ -22,10 +24,72 @@ export const SECONDARY_INFO: {
 };
 /* eslint-enable */
 
+/**
+ * Order is important, rendered from: Latest (top) -> Oldest (bottom)
+ */
+export const TIMELINE_INFO: {
+    date: string;
+    title: string;
+    place?: string;
+    iconbg?: string;
+    titleColor?: string;
+    description?: string;
+    Icon?: FC;
+}[] = [
+    {
+        date: 'May 2021 - August 2021',
+        title: 'Internship @ DS Legends Pte. Ltd.',
+        place: 'Bhopal',
+        description: 'PHP Developer Intern (Full-time)'
+    },
+    {
+        date: 'December 2020 - Feb 2021',
+        title: 'Internship @ FOXAISR',
+        place: 'Bhopal',
+        description: 'NodeJS Developer Intern'
+    },
+    {
+        date: 'May 2020 - June 2020',
+        title: 'Internship @ Think2Exam',
+        place: 'Bhopal',
+        description: 'Full-Stack Developer Intern'
+    },
+    {
+        date: 'December 2019 - Jan 2020',
+        title: 'Internship @ XXYY Studios',
+        place: 'Sikkim',
+        description: 'Backend Developer Intern'
+    },
+    {
+        date: 'August 2018',
+        title: 'Joined College',
+        place: 'Sikkim',
+        description: 'National Institute of Technology, Sikkim'
+    },
+    {
+        date: 'March 2018',
+        title: '12th Boards',
+        place: 'Bhopal',
+        description: 'Kendriya Vidyalaya No. 3, Bhopal'
+    },
+    {
+        date: 'March 2016',
+        title: '10th Boards',
+        place: 'Bhopal',
+        description: 'Kendriya Vidyalaya No. 3, Bhopal'
+    },
+    {
+        date: '29 September 2000',
+        title: 'Birthday',
+        place: 'Mumbai'
+    }
+];
+
 export const PROFILE_IMG = profileImg;
 
 export default {
     PROFILE_IMG,
     PRIMARY_INFO,
+    TIMELINE_INFO,
     SECONDARY_INFO
 };
