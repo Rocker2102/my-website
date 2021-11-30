8b6f3f6a2f3bb49f689f81c15b70bab1439541f3
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-30 12:40:36 +0530

    Add icons in timeline data

--- src/shared/aboutData.ts
+++ src/shared/aboutData.ts
@@ -1,5 +1,10 @@
 import { FC } from 'react';
 
+import CakeIcon from '@mui/icons-material/CakeRounded';
+import WorkIcon from '@mui/icons-material/WorkOutlineRounded';
+import SchoolIcon from '@mui/icons-material/SchoolRounded';
+import AutoStoriesIcon from '@mui/icons-material/AutoStoriesRounded';
+
 import profileImg from '../images/profile.jpg';
 
 /* eslint-disable */
@@ -39,50 +44,58 @@ export const TIMELINE_INFO: {
         date: 'May 2021 - August 2021',
         title: 'Internship @ DS Legends Pte. Ltd.',
         place: 'Bhopal',
-        description: 'PHP Developer Intern (Full-time)'
+        description: 'PHP Developer Intern (Full-time)',
+        Icon: WorkIcon
     },
     {
         date: 'December 2020 - Feb 2021',
         title: 'Internship @ FOXAISR',
         place: 'Bhopal',
-        description: 'NodeJS Developer Intern'
+        description: 'NodeJS Developer Intern',
+        Icon: WorkIcon
     },
     {
         date: 'May 2020 - June 2020',
         title: 'Internship @ Think2Exam',
         place: 'Bhopal',
-        description: 'Full-Stack Developer Intern'
+        description: 'Full-Stack Developer Intern',
+        Icon: WorkIcon
     },
     {
         date: 'December 2019 - Jan 2020',
         title: 'Internship @ XXYY Studios',
         place: 'Sikkim',
-        description: 'Backend Developer Intern'
+        description: 'Backend Developer Intern',
+        Icon: WorkIcon
     },
     {
         date: 'August 2018',
         title: 'Joined College',
         place: 'Sikkim',
         titleColor: 'success',
-        description: 'National Institute of Technology, Sikkim'
+        description: 'National Institute of Technology, Sikkim',
+        Icon: SchoolIcon
     },
     {
         date: 'March 2018',
         title: '12th Boards',
         place: 'Bhopal',
-        description: 'Kendriya Vidyalaya No. 3, Bhopal'
+        description: 'Kendriya Vidyalaya No. 3, Bhopal',
+        Icon: AutoStoriesIcon
     },
     {
         date: 'March 2016',
         title: '10th Boards',
         place: 'Bhopal',
-        description: 'Kendriya Vidyalaya No. 3, Bhopal'
+        description: 'Kendriya Vidyalaya No. 3, Bhopal',
+        Icon: AutoStoriesIcon
     },
     {
         date: '29 September 2000',
         title: 'Birthday',
         place: 'Mumbai',
-        titleColor: 'success'
+        titleColor: 'success',
+        Icon: CakeIcon
     }
 ];
 
