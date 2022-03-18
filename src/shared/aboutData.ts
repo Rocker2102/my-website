40892af068955fa92ec196d85cefeaa9f8564701
Ankush Yadav <ankushyadav9302@gmail.com>
2022-03-18 12:25:50 +0530

    Fix typo

--- src/shared/aboutData.ts
+++ src/shared/aboutData.ts
@@ -12,7 +12,7 @@ import profileImg from '../images/profile.jpg';
 export const PRIMARY_INFO = [
     `I am an enthusiastic full-stack LAMP/MERN web developer who enjoys experimenting
         with new programming concepts & technologies`,
-    `I'm currently working as an SWE intern at Dassault Systèmes.`
+    `I'm currently working as an SWE intern at Dassault Systèmes`
 ];
 
 export const SECONDARY_INFO: {
