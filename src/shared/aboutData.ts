a7a26fbb6b834edbda69190b99660da11bb45f27
Ankush Yadav <ankushyadav9302@gmail.com>
2023-11-11 21:22:19 +0530

    Update about data

--- src/shared/aboutData.ts
+++ src/shared/aboutData.ts
@@ -21,7 +21,7 @@ export const SECONDARY_INFO: {
   'hobbies & interests': [
     'Fan of fantasy, action & adventure movies',
     '❤️ playing online FPS games & football',
-    'I like meeting new people.... over texting 😅'
+    'I like meeting new people.. over texting 😅 > Well, not anymore!'
   ],
   'technical knowledge': [
     `Worked with PHP frameworks like Laravel & Codeigniter for developing REST
